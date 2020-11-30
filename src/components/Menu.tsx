bfe72a412d05ccde5982ac03133bfb06c1871990
cirrus-8691 <diaz.frederic@gmail.com>
2020-11-30 09:32:31 +0100

    add titleframe

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,7 +10,7 @@ export default function Menu(props : Readonly<PageProps>) {
     //console.log("Menu");
 
     return <header className={styles.header}>
-                <div>
+                <div className={styles.titleframe}>
                     <div className={styles.logo}>
                         <img src="./logo.svg" alt="logo" width="64"/>
                     </div>
